714d77d1e5e7dd417e7a74b22135e91540d5cbe9
jxo5716 <joladiplenty@gmail.com>
2024-10-12 17:22:59 -0500

    Revert "Signup"
    This reverts commit e79836d7a87c0b49ca38c898c6169017a06baedc.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
 import React from 'react'
 import NavBar from './Layout/NavBar'
-<<<<<<< Updated upstream
 
-=======
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
-import Home from "./Pages/Home"
-import Game from "./Pages/Game"
-import Signup from './Pages/Signup'
->>>>>>> Stashed changes
 
 function App() {
 
   return (
-   <div className='d-flex  justify-content-center'>
+   <>
     <NavBar/>
-<<<<<<< Updated upstream
     <h1>QUIZOOT</h1>
    </>
-=======
-    <Router>
-      <Routes>
-        <Route exact path = '/Signup' element= {<Signup/>}/>
-        <Route exact path = '/' element= {<Home/>}/>
-        <Route exact path = '/Game' element= {<Game/>}/>
-      </Routes>
-    </Router>
-   </div>
->>>>>>> Stashed changes
   )
 }
 
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default App
