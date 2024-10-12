5734bfd1f18896b4b16691e1ee8e33e6ef52ebdf
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-12 17:51:12 -0500

    added user CRUD(create, delete only) functions

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -1,3 +1,26 @@
-/* const {db } = require('../database/mysqlConnection')
+const { db } = require("../database/mysqlConnection");
 
-const  */
+const createUser = (username, email, profilePhoto, callback) => {
+  const query =
+    "INSERT INTO user (userName, email, profilePhoto) VALUES (?, ? ,? )";
+
+  db.query(query, [username, email, profilePhoto || null], (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results.insertId);
+  });
+};
+
+const deleteUser = (id, callback) => {
+  const query = `DELETE FROM user WHERE id=?`;
+
+  db.query(query, [id], (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results.affectedRows);
+  });
+};
+
+module.exports = { createUser, deleteUser };
