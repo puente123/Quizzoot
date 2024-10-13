83aa5250e991db48f9453a1d431551705f1ab8e0
jxo5716 <joladiplenty@gmail.com>
2024-10-12 22:02:29 -0500

    Routing in Home page done

--- frontend/src/Components/CardWheel.jsx
+++ frontend/src/Components/CardWheel.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import FlashCard from './FlashCard'
+import { Carousel } from 'react-bootstrap'
 
 
-export default function CardWheel() {
-  return (
-	<div>
-	  
-	</div>
-  )
-}
+export default function CardWheel({ deck }) {
+
+	return (
+		<div 
+			style={{
+				width: "800px",
+				border: "4px",
+				height: "300px"
+			}}
+			className='d-flex flex-column align-items-center justify-content-center'
+		>
+			<Carousel slide={false} 
+				style={{width: '600px'}}
+				className=''
+			>
+				{deck.map((card,index) =>(
+					<Carousel.Item className='px-5 mx-5'>
+						<FlashCard front={card.front} back = {card.back}/>
+					</Carousel.Item>
+				))}
+			</Carousel>
+
+		</div>
+	)
+  }
---
+++ frontend/src/Pages/HostGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../Layout/NavBar'
+
+
+export default function HostGame() {
+  return (
+	<div>
+		<NavBar/>
+	</div>
+  )
+}
