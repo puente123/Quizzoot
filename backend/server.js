4a94a26a2fbcf73735ab72c49b47c3cb626e5f7b
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-12 19:42:54 -0500

    Updated server to so it waits to connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const {connectDB} = require("./database/mysqlConnection")
+const { connectDB } = require("./database/mysqlConnection");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2500;
 
-connectDB()
+(async () => {
+  try {
+    await connectDB();
 
-// Basic route
-app.get("/", (req, res) => {
-  res.send("Hello, MERN with MySQL!");
-});
+    // Basic route
+    app.get("/", (req, res) => {
+      res.send("Hello, MERN with MySQL!");
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+    
+  } catch (error) {
+    console.error("Failed to connnect", error);
+  }
+})();
