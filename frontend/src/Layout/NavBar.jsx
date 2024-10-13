5ab2f4883418e163f965d1708ad8d218be08feae
jxo5716 <joladiplenty@gmail.com>
2024-10-13 04:39:27 -0500

    Search Func

--- frontend/src/Layout/NavBar.jsx
+++ frontend/src/Layout/NavBar.jsx
@@ -1,27 +1,53 @@
-import React from 'react'
-import { Container, Navbar} from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Container, Navbar,Nav, Offcanvas, ListGroup, ListGroupItem} from 'react-bootstrap';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useLocation } from 'react-router-dom';
+import Search from '../Components/Search';
 
+export default function NavBar({username }) {
+  const [show,setShow] = useState(false)
 
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  const location = useLocation()
+  
 
-export default function NavBar({username}) {
   return (
-    <>
+    <Nav>
       <Navbar fixed="top" className="bg-body-tertiary">
-        <Container>
+        <Container className='align-items-center justify-content-space-between'>
           <Navbar.Brand href="#home" className='text-dark'>QUIZZOOT</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
               {username ? (
-                  <>Signed in as: <a href="#login">{username}</a></>
+                  <>
+                    <AccountCircleIcon 
+                      className='fs-3'
+                      onClick= {handleShow}
+                    />
+                    <Offcanvas placement='end' show={show} onHide={handleClose} backdrop='static'>
+                      <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Hi {username}</Offcanvas.Title>
+                      </Offcanvas.Header>
+                      <Offcanvas.Body>
+                        <ListGroup variant='flush'>
+                          <ListGroupItem>
+                            My Profile
+                          </ListGroupItem>
+                        </ListGroup>
+                      </Offcanvas.Body>
+                    </Offcanvas>
+                  </>
                 ) : (
                   <a href="/login">Click to sign in</a>
               )}
+                
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </>
+    </Nav>
     
   )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import {React,useState} from 'react'
 import NavBar from '../Layout/NavBar'
 import MiniDeck from '../Components/MiniDeck'
-import { Container,Row,Col } from 'react-bootstrap'
+import { Container,Row,Col,InputGroup,Form } from 'react-bootstrap'
 
 
 
 export default function Home({username}) {
+
+	const [searchText,setSearchText] = useState('')
+
 	const [deckList, setDeckList] = useState([
 		[
 			{
@@ -80,6 +83,15 @@ export default function Home({username}) {
 		]
 	])
 
+
+	const filteredDecks = deckList.flat().filter(deck => 
+		deck.name.toLowerCase().includes(searchText) ||
+		deck.subject.toLowerCase().includes(searchText)
+	);
+
+
+
+	
   return (
 	<div className=' d-flex flex-column vh-100 vw-100 '>
 		<div className='d-flex flex-row align-items-center justify-content-center '>
@@ -89,20 +101,30 @@ export default function Home({username}) {
 			<div className='d-flex flex-row pt-5 mt-3 mx-5 pb-3 align-items-center justify-content-center'>
 				<h2 className='text-white '> Home Page</h2>
 			</div>
+			
+		</div>
+		<div className='position-relative top-0 start-50 mt-3  translate-middle d-flex justify-content-center align-items-center w-50'>
+			<InputGroup size="sm" className=" d-flex  mx-5  align-items-center justify-content-center mb-3 ">
+				<InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
+				<Form.Control
+				aria-label="search"
+				aria-describedby="inputGroup-sizing-sm"
+				placeholder='type to search ...'
+				onChange={(e) => setSearchText(e.target.value.toLowerCase())}
+				/>
+			</InputGroup>
 		</div>
 		<Container className='d-grid'>
-			{deckList.map((subDecks,rowindex)=>(
-				Array.from({ length: Math.ceil(subDecks.length / 5) }, (_, rowIndex) => (
-					<Row className='mb-3' key={`row-${rowIndex}`}>
-						{subDecks.slice(rowIndex * 5, rowIndex * 5 + 5).map((deck, index) => (
-							<Col key={`deck-${rowIndex}-${index}`}>
-								<MiniDeck deck={deck} />
-							</Col>
-						))}
-					</Row>
-				)
-			)))}
-		</Container>	
+		{Array.from({ length: Math.ceil(filteredDecks.length / 5) }, (_, rowIndex) => (
+                    <Row className='mb-3' key={`row-${rowIndex}`}>
+                        {filteredDecks.slice(rowIndex * 5, rowIndex * 5 + 5).map((deck, index) => (
+                            <Col key={`deck-${rowIndex}-${index}`}>
+                                <MiniDeck deck={deck} />
+                            </Col>
+                        ))}
+                    </Row>
+                ))}
+            </Container>	
 	</div>
   )
 }
