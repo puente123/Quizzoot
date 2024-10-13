69ac04ffc7f0aff39873562799c8298724814202
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-12 19:13:08 -0500

    Created userController file that calls database create and delete functions

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -1,20 +1,18 @@
 const { db } = require("../database/mysqlConnection");
 
-const createUser = async (username, email, profilePhoto) => {
+const addUserToDatabase = async (username, email, profilePhoto) => {
   const query =
     "INSERT INTO user (userName, email, profilePhoto) VALUES (?, ? ,? )";
 
   try {
-    const [result] = await db
-      .promise()
-      .query(query, [username, email, profilePhoto || null]);
+    const [result] = await db.promise().query(query, [username, email, profilePhoto || null]);
     return result.insertId;
   } catch (error) {
     throw error;
   }
 };
 
-const deleteUser = async (id) => {
+const deleteUserFromDatabase = async (id) => {
   const query = `DELETE FROM user WHERE id=?`;
 
   try {
@@ -25,4 +23,4 @@ const deleteUser = async (id) => {
   }
 };
 
-module.exports = { createUser, deleteUser };
+module.exports = { addUserToDatabase, deleteUserFromDatabase };
