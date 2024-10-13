40b18c0b1f60c148cfdb7d7dd066e8d3c7a0eb01
Marshal Marks <marshmarks@gmail.com>
2024-10-13 02:04:04 -0500

    Added answer feedback and next question button in quiz

--- frontend/src/Components/Answers.jsx
+++ frontend/src/Components/Answers.jsx
@@ -1,30 +1,62 @@
 import React, { useState } from 'react'
 
-export default function Answers(props) {
+export default function Answers({ locked, setLocked, submitAnswer, nextQuestion, answerChoices, correctAnswer }) {
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     function safeSetSelectedIndex(index) {
-        if(!props.locked)
+        if(!locked)
             setSelectedIndex(index);
     }
 
     // Submits only if an answer has been selected (default is -1)
     function safeSubmit() {
-        if(selectedIndex <= 0 || selectedIndex >= 3)
+        if(selectedIndex < 0 || selectedIndex > 3)
             return;
-        props.submitAnswer(selectedIndex);
+        setLocked(true);
+        submitAnswer(selectedIndex);
+    }
+
+    function safeNextQuestion() {
+        nextQuestion();
+        setSelectedIndex(-1);
+    }
+
+    function SubmitButton() {
+        return (
+            <div className="bg-white p-4 m-2 mt-3 rounded-4 container" style={{cursor: "pointer"}} onClick={() => safeSubmit()}>
+                <h1 className="text-black fs-3 text-center">Submit</h1>
+            </div>
+        )
+    }
+
+    function NextQuestionButton() {
+        return (
+            <div className="bg-white p-4 m-2 mt-3 rounded-4 container" style={{cursor: "pointer"}} onClick={() => safeNextQuestion()}>
+                <h1 className="text-black fs-3 text-center">Next Question</h1>
+            </div>
+        )
+    }
+
+    function answerColor(index) {
+        let output = "bg-white"
+        if(correctAnswer === index && locked)
+            output = "bg-success";
+        if(selectedIndex === index && !locked)
+            output = "bg-info";
+        if(selectedIndex === index && locked && correctAnswer !== index)
+            output = "bg-danger";
+        output += " p-4 m-2 rounded-4 container-md";
+        return output
     }
 
     return (
         <>
-            {props.answerChoices.map((answerChoice, index) => (
-                <div key={index} className={selectedIndex===index ? "bg-info p-4 m-2 rounded-4 container-md" : "bg-white p-4 m-2 rounded-4 container-md"} style={{cursor: "pointer"}} onClick={() => safeSetSelectedIndex(index)}>
+            {answerChoices.map((answerChoice, index) => (
+                <div key={index} className={answerColor(index)} style={{cursor: "pointer"}} onClick={() => safeSetSelectedIndex(index)}>
                     <h1 className="text-black fs-5">{answerChoice}</h1>
                 </div>
             ))}
-            <div className="bg-white p-4 m-2 mt-3 rounded-4 container" style={{cursor: "pointer"}} onClick={() => safeSubmit()}>
-                <h1 className="text-black fs-3 text-center">Submit</h1>
-            </div>
+            {locked ? <NextQuestionButton /> : <SubmitButton />}
         </>
     )
 }
--- frontend/src/Components/Quiz.jsx
+++ frontend/src/Components/Quiz.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import QuizQuestion from './QuizQuestion'
+import { useState } from 'react';
 
-export default function Quiz(props) {
+export default function Quiz({ deck, submitAnswer }) {
+	const [questionNumber, setQuestionNumber] = useState(1);
+	const [locked, setLocked] = useState(false);
+
+    // PLACEHOLDER
+    let correctAnswer = 3;
+    
+    // PLACEHOLDER
+	function getQuestion() {
+		return "This is an example question. What is the answer? This is an example question. What is the answer? This is an example question. What is the answer?";
+	}
+
+	// PLACEHOLDER
+	function getAnswerChoices() {
+		return [
+			"choice1",
+			"choice2",
+			"choice3",
+			"choice4"
+		];
+	}
+
+    function nextQuestion() {
+        setQuestionNumber(questionNumber + 1);
+        setLocked(false);
+    }
+
     return (
-        <QuizQuestion />
+        <QuizQuestion 
+				questionNumber={questionNumber} 
+				question={getQuestion()} 
+				answerChoices={getAnswerChoices()}
+                correctAnswer={correctAnswer} 
+				locked={locked}
+                setLocked={setLocked}
+				submitAnswer={submitAnswer}
+                nextQuestion={nextQuestion} />
     )
 }
